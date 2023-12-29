e91d07334f84f79fcdf56b8f0a971c484985c9f3
Karu <fuskskitmail@gmail.com>
2023-12-29 10:20:44 +0100

    works2

--- app/Http/Controllers/Front/ProductController.php
+++ app/Http/Controllers/Front/ProductController.php
@@ -56,6 +56,7 @@ class ProductController extends Controller
                 'categoryProducts',
                 'categories',
                 'homeSliderBanner',
+                'url',
             ));
 
         } else {
--- public/front/js/front.js
+++ public/front/js/front.js
@@ -10,35 +10,7 @@ $(document).ready(function () {
         this.form.submit();
     });
 
-    $("#sort").on("change", function() {
-        var sort = $(this).val();
 
-        var url = $("#url").val();
-        $.ajax({
-            url: url,
-            method: "post",
-            data: {
-                fabric: fabric,
-                sleeve: sleeve,
-                pattern: pattern,
-                fit: fit,
-                occasion: occasion,
-                sort: sort,
-                url: url
-            },
-            success: function(data) {
-                $(".filter_products").html(data);
-            }
-        });
-    });
-
-    function get_filter(class_name) {
-        var filter = [];
-        $("." + class_name + ":checked").each(function() {
-            filter.push($(this).val());
-        });
-        return filter;
-    }
     // --------------- END --------------
 
     // ------- product card start -------
